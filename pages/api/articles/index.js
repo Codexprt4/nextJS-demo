3cac88d86d35d17b92fa829de001ad89614b0b2b
codexprt4 <codexprt4@gmail.com>
2021-08-09 15:55:26 +0530

    "test commit"

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -1,5 +1,9 @@
 import { articles } from "../../../data";
 
+/**
+ * These data can be from DB or Static file
+ */
+
 export default function handler(req, res) {
   res.status(200).json(articles);
 }
